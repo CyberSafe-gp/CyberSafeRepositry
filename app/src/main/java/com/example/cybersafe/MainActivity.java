9c2e11aff57bd5768d255d1a171b1098a83197b0
Lujain Z <55849375+Lujain993@users.noreply.github.com>
2021-02-02 00:00:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cybersafe/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.cybersafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
+}
