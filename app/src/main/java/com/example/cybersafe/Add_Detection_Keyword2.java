94f368e90a6e73bfc4b69da1de27b49eb82e1a64
Shahad <ShahadAliM7@gmail.com>
2021-02-06 23:23:43 +0300

    update

---
+++ app/src/main/java/com/example/cybersafe/Add_Detection_Keyword2.java
@@ -0,0 +1,14 @@
+package com.example.cybersafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_Detection_Keyword2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__detection__keyword2);
+    }
+}
---
+++ app/src/main/java/com/example/cybersafe/Add_NewChild.java
@@ -0,0 +1,14 @@
+package com.example.cybersafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_NewChild extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__new_child);
+    }
+}
