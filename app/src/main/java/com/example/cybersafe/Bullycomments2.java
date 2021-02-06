59a07b31b93275bb2c66896de8db5bda53e491f9
saraalfayez <isaraalfayez@gmail.com>
2021-02-06 21:27:58 +0300

    sara h

---
+++ app/src/main/java/com/example/cybersafe/Bullycomments2.java
@@ -0,0 +1,14 @@
+package com.example.cybersafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bullycomments2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bullycomments2);
+    }
+}
