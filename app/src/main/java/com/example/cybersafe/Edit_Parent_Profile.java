d80c6bc92e40c0e15ebddb4fd848f29c735b640b
Shahad <ShahadAliM7@gmail.com>
2021-02-26 01:54:07 +0300

    update

--- app/src/main/java/com/example/cybersafe/Edit_Parent_Profile.java
+++ app/src/main/java/com/example/cybersafe/Edit_Parent_Profile.java
@@ -1,14 +1,152 @@
 package com.example.cybersafe;
 
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.cybersafe.Objects.Parent;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Edit_Parent_Profile extends AppCompatActivity {
+    //EditText firstName;
+    // String fName;
+    // EditText lastName;
+    //EditText email;
+    // public Button chngeP;
+    //public Button editB;
+    // public static final String TAG = "TAG";
+    private String userId;
+
+
+    private FirebaseUser Cuser;
+    private static final String TAG = Edit_Parent_Profile.class.getSimpleName();
+    private Button btEdit;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference g =  FirebaseDatabase.getInstance().getReference("Parents");
+
+    private EditText editTextfName;
+    private EditText editTextlName;
+    private EditText editTextEmail;
+    public TextView  FnamefromDB,LnamefromDB, emailfromDB;
+    String userEmail,Fname,lName;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit__parent__profile);
+        editTextfName = findViewById(R.id.firstNameB);
+        editTextlName = findViewById(R.id.lastNameB);
+        editTextEmail = findViewById(R.id.EditEmailAddressB);
+        btEdit=(Button)findViewById(R.id.editButton);
+
+        Cuser = FirebaseAuth.getInstance().getCurrentUser();
+        g.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    com.example.cybersafe.Objects.Parent us = postSnapshot.getValue(Parent.class);
+                    if (us.getEmail().equals(Cuser.getEmail())) {
+                        userEmail = us.getEmail();
+                        userId = us.getParent_id();
+                        Fname=us.getFirstName();
+                        lName = us.getLastName();
+                        userId = us.getParent_id();
+                        editTextfName.setText(Fname);
+                        editTextlName .setText(lName);
+                        editTextEmail.setText(userEmail);
+
+                    }
+                }
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        btEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View view) {
+                updateInfo();
+            }
+        });
     }
+            private void updateInfo() {
+
+
+                DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("users").child(userId);
+
+                final String fname = editTextfName.getText().toString().trim();
+                final String lastname = editTextlName.getText().toString().trim();
+                final String email = editTextEmail.getText().toString().trim();
+
+
+                if (fname.isEmpty()) {
+                    editTextfName.setError("User name is required");
+                    editTextfName.requestFocus();
+
+                    return;
+                }
+                if (fname.isEmpty()) {
+                    editTextlName.setError("Field cant be empty");
+                    editTextlName.requestFocus();
+                    return;
+                }
+
+
+                if (!fname.matches("[a-zA-Z]+")) {
+                    editTextfName.setError("first name must contains letter");
+                    editTextfName.requestFocus();
+
+                    return;
+                }
+
+
+                if (!lastname.matches("[a-zA-Z]+")) {
+                    editTextlName.setError("last name must contains letter");
+                    editTextlName.requestFocus();
+
+                    return;
+                }
+
+                if (email.isEmpty()) {
+                    editTextEmail.setError("Email is required");
+                    editTextEmail.requestFocus();
+                    return;
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    editTextEmail.setError("Please provide valid email");
+                    editTextEmail.requestFocus();
+                    return;
+                }
+
+                Parent p = new Parent(fname, lastname, email, userId);
+                userRef.setValue(p);
+                Toast.makeText(this,"Information has been updated successfully", Toast.LENGTH_LONG).show();
+
+
+            }
 }
+
+
+
+
+
