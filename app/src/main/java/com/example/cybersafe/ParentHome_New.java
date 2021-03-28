73aaf37d8bec8fe2ee4d606f70238b418c5b90eb
luulu <luuluuomar@gmail.com>
2021-03-29 00:23:52 +0300

    bottom nav

--- app/src/main/java/com/example/cybersafe/ParentHome_New.java
+++ app/src/main/java/com/example/cybersafe/ParentHome_New.java
@@ -1,5 +1,7 @@
 package com.example.cybersafe;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.Window;
@@ -7,6 +9,7 @@ import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -17,40 +20,38 @@ public class ParentHome_New extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getSupportActionBar().hide();
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_parent_home__new);
         bottomNavigationView = findViewById(R.id.bottom_nav);
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        bottomNavigationView.setItemTextColor(ColorStateList.valueOf(Color.BLACK));
+        if (savedInstanceState == null) {
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.addFragmentLayout, new HomeFragment()).commit();
+
+        }
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    HomeFragment homeFragment = new HomeFragment();
-                    FragmentManager fragmentManager = getSupportFragmentManager();
-                    fragmentManager.beginTransaction().replace(R.id.addFragmentLayout, homeFragment, homeFragment.getTag()).commit();
-                    return true;
-
-                case R.id.navigation_dashboard:
-                    DashboradFragment dashboradFragment = new DashboradFragment();
-                    FragmentManager fragmentManager1 = getSupportFragmentManager();
-                    fragmentManager1.beginTransaction().replace(R.id.addFragmentLayout, dashboradFragment, dashboradFragment.getTag()).commit();
-                    return true;
-
-                case R.id.navigation_notifications:
-                    NotificationFragment notificationFragment = new NotificationFragment();
-                    FragmentManager fragmentManager2 = getSupportFragmentManager();
-                    fragmentManager2.beginTransaction().replace(R.id.addFragmentLayout, notificationFragment, notificationFragment.getTag()).commit();
-                    return true;
-            }
-            return false;
+            = item -> {
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.navigation_dashboard:
+                fragment = new DashboradFragment();
+                break;
+            case R.id.navigation_notifications:
+                fragment = new NotificationFragment();
+                break;
         }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.addFragmentLayout, fragment).commit();
+        return true;
     };
 }
 
