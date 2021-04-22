f1875422227c22fb7a9ff49e3f321860d392a877
lenaah <lmiaih1@gmail.com>
2021-04-22 18:52:47 +0300

    Lenaah

--- app/src/main/java/com/example/cybersafe/Admin_School.java
+++ app/src/main/java/com/example/cybersafe/Admin_School.java
@@ -18,10 +18,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class Admin_School extends AppCompatActivity {
-    DatabaseReference SchoolM = FirebaseDatabase.getInstance().getReference().child("SchoolManagers");
-    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-    private String userID = user.getUid();
+
     private TextView text;
+    String userID;
 
 
 
@@ -30,41 +29,51 @@ public class Admin_School extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin__school);
 
-SchoolM.addValueEventListener(new ValueEventListener() {
-    @Override
-    public void onDataChange(@NonNull DataSnapshot snapshot) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null) {
+             userID = user.getUid();
+             System.out.println("userID "+userID);
+        }else {
+            Intent intent = new Intent(Admin_School.this, Interface.class);
+            startActivity(intent);
 
-        for (DataSnapshot childrf : snapshot.getChildren()) {
-            SchoolManager SchoolM = childrf.getValue(SchoolManager.class);
-            String user_id = SchoolM.getSchoolManager_id();
-            System.out.println("1111111");
-            if (user_id.equals(userID)) {
-                System.out.println("22222222");
-                if(SchoolM.getAdmin().equals("Confirm")){
-                    System.out.println("333333333");
-
-                    Toast.makeText(Admin_School.this, "School Manager registered has been confirmed Successfully", Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(Admin_School.this,SchoolHome_new.class);
-                    startActivity(intent);
-                }
-                    else{
-                    text.setText(" ");
-                }
-                }
-            else {
-
-                text.setText(" ");
-            }
         }
 
+        DatabaseReference SchoolM = FirebaseDatabase.getInstance().getReference().child("SchoolManagers");
+        SchoolM.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot childrf : snapshot.getChildren()) {
+                        SchoolManager SchoolM = childrf.getValue(SchoolManager.class);
+                        String user_id = SchoolM.getSchoolManager_id();
 
-    }
+                        System.out.println("1111111");
+                        if (user_id.equals(userID)) {
+                            System.out.println("22222222");
+                            System.out.println("@@@@@ " + SchoolM.getAdmin());
+                            if (SchoolM.getAdmin().equals("Confirm")) {
+                                System.out.println("333333333");
 
-    @Override
-    public void onCancelled(@NonNull DatabaseError error) {
+                                Toast.makeText(Admin_School.this, "School Manager registered has been confirmed Successfully", Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(Admin_School.this, SchoolHome_new.class);
+                                startService(new Intent(Admin_School.this, ServiceSM.class));
+                                startActivity(intent);
+                            } else {
+                            }
+                        } else {
 
-    }
-});
+                        }
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
 
